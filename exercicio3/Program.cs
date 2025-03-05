cc03b0e8c5f79528d12a81f76936fe09771afef8
joseandery <joseandery2201@gmail.com>
2025-03-05 14:53:25 -0300

    exercicio3.cs ok

---
+++ exercicio3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite o número do funcionário");
+int num_func = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o número de horas trabalhadas pelo funcionário {0}", num_func);
+int horas_trabalhadas = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor que o funcionário {0} recebe por hora trabalhada", num_func);
+double valor_hora = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"NUMBER = {num_func}");
+Console.WriteLine($"SALARY = U$ {(horas_trabalhadas*valor_hora):F2}");
