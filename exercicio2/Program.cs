975487b013591f30f970856e5319d982a40777a0
joseandery <joseandery2201@gmail.com>
2025-03-05 14:50:02 -0300

    exercicio2.cs ok

---
+++ exercicio2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite o valor de A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor de B");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor de C");
+int C = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor de D");
+int D = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"DIFERENÇA = {(A*B)-(C*D)}");
