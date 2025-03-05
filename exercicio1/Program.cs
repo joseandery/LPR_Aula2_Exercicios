56f8de0d3c0975aa00f4ed3376f74c23b77f120b
joseandery <joseandery2201@gmail.com>
2025-03-05 14:44:37 -0300

    exercicio1.cs ok

---
+++ exercicio1/Program.cs
@@ -0,0 +1,8 @@
+﻿const double pi = 3.14159;
+double raio;
+
+Console.WriteLine("Digite o raio do círculo");
+raio = double.Parse(Console.ReadLine());
+
+double area = raio*raio*pi;
+Console.WriteLine($"A={area:F4}");
